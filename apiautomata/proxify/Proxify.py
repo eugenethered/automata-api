412ea3c006076937920b756a2a607f4c678aa2f8
Eugene the Red <me@eugenethered.com>
2022-07-29 16:57:59 +0100

    simple cache for proxy calls

--- apiautomata/proxify/Proxify.py
+++ apiautomata/proxify/Proxify.py
@@ -3,6 +3,7 @@ from coreutility.date_utility import get_utc_timestamp
 
 
 class Proxify:
+    cached_calls = {}
 
     def __init__(self, url):
         self.url = url
@@ -11,7 +12,11 @@ class Proxify:
 
     @classmethod
     async def get_end_point_status(cls, obj):
-        async with aiohttp.ClientSession() as session:
-            async with session.get(obj.url) as response:
-                response_status = response.status
-                obj.status = response_status
+        if obj.url in Proxify.cached_calls.keys():
+            obj.status = Proxify.cached_calls[obj.url]
+        else:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(obj.url) as response:
+                    response_status = response.status
+                    obj.status = response_status
+                    Proxify.cached_calls[obj.url] = response_status
