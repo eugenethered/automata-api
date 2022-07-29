f884729dae2f6de4a4ad9e38809f4c8fc7030117
Eugene the Red <me@eugenethered.com>
2022-07-29 18:04:35 +0100

    cosmetic

--- simulations/serve/__init__.py
+++ simulations/serve/__init__.py
@@ -7,7 +7,7 @@ from apiautomata.server.AutomataAPIServer import AutomataAPIServer
 if __name__ == '__main__':
 
     options = {
-        'REDIS_SERVER_ADDRESS': '192.168.1.90',
+        'REDIS_SERVER_ADDRESS': '10.104.71.60',
         'REDIS_SERVER_PORT': 6379,
         'API_SERVER_HOST': '127.0.0.1',
         'API_SERVER_PORT': 8000,
@@ -17,8 +17,8 @@ if __name__ == '__main__':
         'TRADE_TRANSFORMATIONS_KEY': '<MARKET>:transformation:trade',
         'EXCHANGE_RATE_TIMESERIES_KEY': '<MARKET>:time-series:exchange-rate:{}',
         'EXCHANGE_RATE_TIMESERIES_RETENTION': 360000,
-        'PROCESS_RUN_PROFILE_KEY': '{}:process:run-profile:{}',
-        'PROCESS_KEY': '{}:process:status:{}',
+        'PROCESS_RUN_PROFILE_KEY': '<MARKET>:process:mv:run-profile',
+        'PROCESS_KEY': '<MARKET>:process:mv:status',
         'PROCESS_RUN_COMMAND': 'echo "running: {process-name}"',
         'VERSION': '0.0.1'
     }
